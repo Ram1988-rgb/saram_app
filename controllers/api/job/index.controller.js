a7168a113f09e549fa910a4f5ed193bd46de9962
rajeshyadav0542 <rajesh.yadav754@gmail.com>
2020-05-30 11:47:32 +0530

    today

--- controllers/api/job/index.controller.js
+++ controllers/api/job/index.controller.js
@@ -3,37 +3,6 @@ const constant = require('../../../config/constant');
 const jwt = require('jsonwebtoken');
 const HttpStatus = require('http-status');
 module.exports = {
-<<<<<<< HEAD
-	list : async (req, res) =>{
-		var result = {};
-		model.job.aggregate([
-			{
-				$match :
-				{
-					deleted_at : 0,
-					status : true
-				}
-			},
-			{
-				$lookup : 
-				{
-					from : "users",
-					localField : "_id",
-					foreignField : "user_id",
-					as : "user_data"
-				}
-			}
-		],function(error, record){
-			result.status = HttpStatus.OK;
-			result.data = record;
-			res.send(result);
-		});
-	},
-	
-	apply : async (req, res) => {
-		var result = {};
-		
-=======
 	list : async (req, res) =>{		
 		const resut_data = await model.job.find({ status : true, deleted_at : 0})
 		const result = {};		
@@ -41,7 +10,6 @@ module.exports = {
 		result.data = resut_data;
 		result.status = status;
 		res.status(status).send(result);
->>>>>>> d4a69d4ca3f3461d3d64c59a5f8e83102363531a
 	}
 		
 }
