d4a69d4ca3f3461d3d64c59a5f8e83102363531a
kumar <kkaushal@ongraph.com>
2020-05-30 11:34:34 +0530

    changes

--- middleware/utils.js
+++ middleware/utils.js
@@ -1,21 +1,30 @@
 const jwt = require('jsonwebtoken');
 const constant = require('../config/constant');
-module.exports = {
-  validateToken: (req, res, next) => {
+
+  async function validateToken(req, res, next){
     const authorizationHeaader = req.headers.authorization;
     let result;
     if (authorizationHeaader) {		
       const token = authorizationHeaader;
       try {
-			result = jwt.verify(token, constant.JWT_SECRET);
-			next();
+        result = jwt.verify(token, constant.JWT_SECRET);
+        if(result && result.data && result.data._id){
+          req.USERID =result.data._id;
+          next();
+        }else{
+          result = { 
+            error: `Authentication error. Invalid token.`,
+            status: 401 
+          };
+          res.status(401).send(result);
+        }
       } catch (err) {
         //throw new Error(err);
         result = { 
-			error: `Authentication error. Invalid token.`,
-			status: 401 
-		  };
-		  res.status(401).send(result);
+          error: `Authentication error. Invalid token.`,
+          status: 401 
+		    };
+		    res.status(401).send(result);
       }
     } else {
       result = { 
@@ -25,4 +34,15 @@ module.exports = {
       res.status(401).send(result);
     }
   }
-};
+
+  async function generate_token(data){
+    const payload = { data : data, exp : Math.floor(Date.now() / 1000) + (60 * 60 * 24)};
+    const secret = constant.JWT_SECRET;
+    const token = jwt.sign(payload, secret);
+    return token;
+  }
+
+  module.exports = {
+    validateToken,
+    generate_token
+  }
--- routes/api/job.routes.js
+++ routes/api/job.routes.js
@@ -1,5 +1,5 @@
 var indexController = require('../../controllers/api/job/index.controller');
-var validateToken = require('../../middleware/utils').validateToken;
+var {validateToken} = require('../../middleware/utils');
 module.exports = function(router) {
 	router.get('/api/job/list', validateToken, indexController.list);
 }
